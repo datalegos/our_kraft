e2e79da6418e32ead39408297c2f288a6f005d99
sumanth-20021221 <sumanthgadepalli21@gmail.com>
2025-07-22 15:44:53 +0530

    commiting chatbot

--- sumanth/news_aggregator/tasks/user_types/__init__.py
+++
@@ -1,9 +0,0 @@
-from .anonymous_user import AnonymousUserPromptGenerator
-from .new_user import NewUserPromptGenerator
-from .existing_user import ExistingUserPromptGenerator
-
-__all__ = [
-    'AnonymousUserPromptGenerator',
-    'NewUserPromptGenerator', 
-    'ExistingUserPromptGenerator'
-] 
